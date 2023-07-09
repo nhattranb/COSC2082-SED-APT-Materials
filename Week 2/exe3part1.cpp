1a32ffe49a676fae69b5a89aa5fd8e451b0b0cd3
nhattranb <s3926629@rmit.edu.vn>
2023-07-09 17:48:58 +0700

    add codes for exercise 3 part 1

--- Week 2/exe3part1.cpp
+++ Week 2/exe3part1.cpp
@@ -6,5 +6,11 @@ int main() {
     string str;
     cout << "Please type in your string \n";
     cin >> str;
-    
+    char string[] = str;
+    cout << "Individual characters from given string: \n";
+    //Iterate through the string and display individual character  
+    for(int i = 0; i < str.length(); i++) {  
+        cout << "%c " << string[i];  
+    }  
+    return 0;  
 }
