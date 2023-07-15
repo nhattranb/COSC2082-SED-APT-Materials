6ceb39602f0a599297032c56db71eb9ae41618dc
nhattranb <s3926629@rmit.edu.vn>
2023-07-15 20:42:36 +0700

    add code for w3

--- Week 3/question2.cpp
+++ Week 3/question2.cpp
@@ -1,3 +1,23 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
+
+class Student {
+    public:
+    int score;
+    string name;
+    Student(string myName, int myScore) {
+        name = myName;
+        score = myScore;  
+    }
+};
+
+int main() {
+    cout << "It's Object-Oriented Programming Time! \n";
+    Student student1("Nhat", 71);
+    Student student2("Quang", 80);
+    cout << student1.name << " " << student2.name << "\n";
+    cout << student1.name << " " << student2.name << "\n";
+    return 0;
+}
