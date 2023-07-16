e8e31a3b69188ba2093da662c1cecc580d8b90f9
nhattranb <s3926629@rmit.edu.vn>
2023-07-16 20:27:27 +0700

    modify unfinished codes

---
+++ Week 3/question4.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+class Cat {
+    public:
+    string name;
+    int age;
+    Cat(string thisName, int thisAge) {
+        name = thisName;
+        age = thisAge;
+    }
+};
+
+class Person {
+    public:
+    Cat myCat;
+    string myName;
+    Person(Cat cat, string name) {
+        cat = myCat;
+        name = myName;
+    }
+};
+
+int main() {
+    Person myPer1("Puppy", 6, "Nhat");
+    Person myPer2("Tom", )
+}
