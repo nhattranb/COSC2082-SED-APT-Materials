484d2a2f920b5605bdf5a4d21e5e0574c48b9228
nhattranb <s3926629@rmit.edu.vn>
2023-07-23 19:25:05 +0700

    add new files for w4

---
+++ Week 4/question2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+class Account {
+    public:
+    string name;
+    float amount;
+
+    Account(string myName, float myAmount) {
+        name = myName;
+        account = myAmount;
+    }
+
+    public:
+    void withdraw(float num) {
+        
+    }
+};
+
+int main() {
+    cout << "Welcome to the program! \n";
+    Account bankAcc1("Nhat Tran", 6790000);
+    Account bankAcc2("Anna Felipe", 7520000);
+    Account bankAcc3("Ling Huo", 7670000);
+}
