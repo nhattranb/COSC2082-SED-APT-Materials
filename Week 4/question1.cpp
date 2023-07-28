98d90d45ca73ad8f50836c0969c3ac04627a883b
nhattranb <s3926629@rmit.edu.vn>
2023-07-28 09:58:49 +0700

    add codes for w4

--- Week 4/question1.cpp
+++ Week 4/question1.cpp
@@ -1,20 +1,38 @@
 #include <iostream>
 #include <fstream>
-#include <string>
+#include <cstring>
+#include <cmath>
 using namespace std;
 
 class ComplNum {
 private:
-    int real;
+    double real;
     double imag;
 
-int getReal() {
+ComplNum(int real, double imag) {
+    this->real = real;
+    this->imag = imag;
+}
+
+public:
+double getReal() {
 	return this->real;
 }
 void setReal(int real) {
 	this->real = real;
 }
 
+ComplNum operator-(const ComplNum& other) const {
+    return ComplNum(real - other.real, imag - other.imag);
+}
+
+ComplNum operator - (double num) const {
+    return ComplNum(real - other.real, imag - other.imag);
+}
+
+friend ComplNum operator-(double num, double imag) const {
+    return ComplNum(real - other.real, imag - other.imag);
+}
 
 double getImag() {
     return this->imag;
@@ -29,4 +47,10 @@ public:
 
 int main() {
     cout << "Code testing \n";
+    ComplNum num1(3.0, 4.0);
+    ComplNum num2(1.5, 2.5);
+
+    ComplNum result1 = num1 - num2;
+    ComplNum result2 = num1 - 2.0;
+    ComplNum result3 = 5.0 - num2;
 }
