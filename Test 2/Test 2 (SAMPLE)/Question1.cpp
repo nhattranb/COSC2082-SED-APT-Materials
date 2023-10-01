08dd2b7e52970152f57e7893e489b5bf21d31019
nhattranb <s3926629@rmit.edu.vn>
2023-10-01 12:39:06 +0700

    increase the salary

--- Test 2/Test 2 (SAMPLE)/Question1.cpp
+++ Test 2/Test 2 (SAMPLE)/Question1.cpp
@@ -18,7 +18,7 @@ class Employee {
     }
 
     string toString() {
-        return "Employee name: " + name + "has a salary of " + to_string(salary) + "\n";
+        return "Employee name: " + name + " has a salary of " + to_string(salary) + "\n";
     }
 
     void showInfo() {
@@ -36,7 +36,7 @@ class Manager: public Employee {
     }
 
     string toString() {
-        return "Manager name: " + name + " has a salary of" + to_string(salary) + " and has an allowance of" + to_string(allowance) + "\n";
+        return "Manager name: " + name + " has a salary of " + to_string(salary) + " and has an allowance of " + to_string(allowance) + "\n";
     }
 
     void showInfo() {
@@ -52,29 +52,38 @@ int main() {
     cout << "Units are in DOLLARS. \n";
     
     Employee e1("Nhat Tran Minh", 100);
+    Employee e2("Huy Nguyen", 200);
     e1.showInfo();
+    e2.showInfo();
     cout << "\n";
 
-    Manager m1("Anna Lyza Felipe", 280, 130);
+    Manager m1("Anna Lyza Felipe", 300, 150);
+    Manager m2("Quang Tran", 400, 200);
     m1.showInfo();
+    m2.showInfo();
     cout << "\n";
 
-    Manager *mana = new (nothrow) Manager[5] {
-        Manager("Tom Huynh", 350, 150),
-        Manager("Ling Huo Chong", 260, 120),
-        Manager("Long Nguyen", 290, 145),
-        Manager("Jonathan Crellin", 300, 160),
-        Manager("Joshua Dwight", 325, 125)
+    Manager *mana = new (nothrow) Manager[10] {
+        Manager("Tom Huynh", 500, 300),
+        Manager("Ling Huo Chong", 300, 170),
+        Manager("Long Nguyen", 300, 150),
+        Manager("Jonathan Crellin", 350, 200),
+        Manager("Joshua Dwight", 350, 150),
+        Manager("Tri Dang", 300, 150),
+        Manager("Thanh Tran", 400, 200),
+        Manager("Bao Nguyen", 250, 150),
+        Manager("Sam Goundar", 250, 150),
+        Manager("Ushik Khwakhali", 450, 200)
     };
     if (mana == nullptr){
         cout << "No data existed. \n";
         return -1;
     }
     double totalIncome = 0;
-    for(int i=0; i<5; i++) {
+    for(int i=0; i<10; i++) {
         totalIncome = totalIncome + mana[i].totalIncome();
     }
-    cout << "Average of the managers : " << totalIncome/5 << "\n";
+    cout << "Average of the managers : " << totalIncome/10 << "\n";
     delete [] mana;
     return 0;
 }
